9c5ab18f2380505761e62540d0d5337ae98df9e5
Zhen Jin <zhen.jin@wework.com>
2018-02-09 00:25:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(order)
-  colors = ["Red","Orange","Yellow","Green","Blue","Purple"]
+  colors = ["Red","Orange","Yellow","Green","Blue","Indigo","Violet"]
 end
