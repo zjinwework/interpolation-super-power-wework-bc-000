986f26fa64361347b9d8ab12109d85e94bc4937d
Zhen Jin <zhen.jin@wework.com>
2018-02-09 00:29:41 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,5 @@
 
 def display_rainbow(colors)
   # colors = ["Red","Orange","Yellow","Green","Blue","Indigo","Violet"]
-  
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
